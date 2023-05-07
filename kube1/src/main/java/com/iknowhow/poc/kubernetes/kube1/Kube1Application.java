2c6380b0619dd00e9dd69411d214b18fa5f435c3
Dimitris Pappas <dpappas@outlook.com>
2023-05-07 13:15:30 +0300

    Removed Ribbon dependency since it's not advised anymore and we already use Spring Cloud Load Balancer Changed spring.cloud.kubernetes.discovery.discovery-server-url from IP to service name of the discovery server

--- kube1/src/main/java/com/iknowhow/poc/kubernetes/kube1/Kube1Application.java
+++ kube1/src/main/java/com/iknowhow/poc/kubernetes/kube1/Kube1Application.java
@@ -3,13 +3,11 @@ package com.iknowhow.poc.kubernetes.kube1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
-@RibbonClient(name = "kube1-service")
 public class Kube1Application {
 
     public static void main(String[] args) {
--- kube2/src/main/java/com/iknowhow/poc/kubernetes/kube2/Kube2Application.java
+++ kube2/src/main/java/com/iknowhow/poc/kubernetes/kube2/Kube2Application.java
@@ -3,13 +3,11 @@ package com.iknowhow.poc.kubernetes.kube2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
-@RibbonClient(name = "kube2-service")
 public class Kube2Application {
 
     public static void main(String[] args) {
